2c37bb04dea97d07d96c5584327e121edcb2af57
Leo Osvald <leo.osvald@gmail.com>
2005-01-20 05:52:45 -0500

    Commit: ceilfloor.cpp

---
+++ learning-cpp/ceilfloor.cpp
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <iostream.h>
+#include <conio.h>
+int main() {
+    float a = 2.54, b = 5.03, c = 1.98;
+    cout << " ceil " << ceil(a) << " floor " << floor(a) << endl;
+    cout << " ceil " << ceil(b) << " floor " << floor(b) << endl;
+    cout << " ceil " << ceil(c) << " floor " << floor(c) << endl; 
+    getch();
+    return 0;
+}     
